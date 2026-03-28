b3d728eaede07695a13dd330317ea7a3f7b96fb3
Snehashis <mandalsnehashis35@gmail.com>
2026-03-28 12:36:21 +0530

    Add TestNG configuration for cross-browser testing and integrate ExtentReports
    # Conflicts:
    #	Automation_Testing/selenium-ui-automation/test-output/SparkReport/Spark.html

--- Automation_Testing/selenium-ui-automation/src/test/java/com/freecrm/automation/runners/TestRunnerContacts.java
+++ Automation_Testing/selenium-ui-automation/src/test/java/com/freecrm/automation/runners/TestRunnerContacts.java
@@ -2,10 +2,17 @@ package com.freecrm.automation.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"src/test/resources/featureFiles/contacts"},
-        glue = {"com.freecrm.automation.stepDefinitions.contacts"},
+        glue = {"com.freecrm.automation.stepDefinitions.contacts", "com.freecrm.automation.hooks"},
         tags = "@TC_Contacts",
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         dryRun = false)
 public class TestRunnerContacts extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
